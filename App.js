e18223a9de1c0873b882a3dbab7b02d6f96b38d8
kmarks2013 <kmarks2013@gmail.com>
2020-01-10 13:48:50 -0500

    added components creen to app.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen'
 
 const navigator = createStackNavigator(
   {
