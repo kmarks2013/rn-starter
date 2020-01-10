af17f25c37ed6082b4fe8a44c6390a80e494ef35
kmarks2013 <kmarks2013@gmail.com>
2020-01-10 15:03:27 -0500

    added new text for excercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen = () => {
-    const greeting = <Text>What's up?</Text>
+    // const greeting = <Text>What's up?</Text>
+    const myName = "Kenny"
     return (
     <View>
-    <Text style={styles.textStyle}>This is the components screen</Text>
+    {/* <Text style={styles.textStyle}></Text>
     <Text> Hi there!</Text>
-    {greeting}
+    {greeting} */}
+    <Text style={styles.textStyle}> Getting started wtih react native!</Text>
+    <Text style={styles.greetingStyle}> My name is {myName} </Text>
     </View>
     )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    greetingStyle: {
+        fontSize: 20
     }
+
 })
 
 export default ComponentsScreen
