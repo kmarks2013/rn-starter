ca6a72dcea51420613befa166d2f0f5f0e55f5fc
kmarks2013 <kmarks2013@gmail.com>
2020-01-13 17:32:18 -0500

    Adds empty stylesheet and exports component.

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,3 +4,7 @@ import { View, Text, StyleSheet } from 'react-native'
 const ListScreen = () => {
     return <Text> List Screen </Text>
 }
+
+const styles = StyleSheet.create({})
+
+export default ListScreen
