a2d410c88037cb6e8a7935db238144b50f3e7e45
kmarks2013 <kmarks2013@gmail.com>
2020-01-14 16:20:20 -0500

    option 2 for inserting keys

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,19 +4,20 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 const ListScreen = () => {
     const friends = [
         // option 1 for adding keys
-        { name: 'Friend #1', key: "1"},
-        { name: 'Friend #2', key: "2"},
-        { name: 'Friend #3', key: "3"},
-        { name: 'Friend #4', key: "4"},
-        { name: 'Friend #5', key: "5"},
-        { name: 'Friend #6', key: "6"},
-        { name: 'Friend #7', key: "7"},
-        { name: 'Friend #8', key: "8"},
-        { name: 'Friend #9', key: "9"}
+        { name: 'Friend #1', /*key: "1"*/},
+        { name: 'Friend #2', /*key: "2"*/},
+        { name: 'Friend #3', /*key: "3"*/},
+        { name: 'Friend #4', /*key: "4"*/},
+        { name: 'Friend #5', /*key: "5"*/},
+        { name: 'Friend #6', /*key: "6"*/},
+        { name: 'Friend #7', /*key: "7"*/},
+        { name: 'Friend #8', /*key: "8"*/},
+        { name: 'Friend #9', /*key: "9"*/}
     ]
 
     return (
         <FlatList 
+            keyExtractor= {friend => friend.name}
             data={friends} 
             renderItem={({item}) =>{
                 // destructures element from this: element === {item: {name: 'Friend #1'}, index : 0}
