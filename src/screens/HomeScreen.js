4ab2f4de5a34027d39a071e44a0115e2ee8ea88e
kmarks2013 <kmarks2013@gmail.com>
2020-01-14 17:03:22 -0500

    adds onPress event listener with console log

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,7 +5,10 @@ const HomeScreen = () => {
   return (
     <View>
       <Text style={styles.text}>Hello World! </Text>
-      <Button title="Go to Components Demo" />
+      <Button 
+        onPress={() => console.log('button pressed')}
+        title="Go to Components Demo" 
+      />
     </View>
   )
 };
