a50f793dc534a7876b4a4a2f518fa32a109d5f9f
kmarks2013 <kmarks2013@gmail.com>
2020-01-23 16:08:42 -0500

    fixes stylesheet error and adds fontsize

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { View, Text, Stylesheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>
+    return <Text style={styles.textStyle}>Image Screen</Text>
 }
 
-const styles = StyleSheet.create()
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 50
+    }
+})
 
 export default ImageScreen
