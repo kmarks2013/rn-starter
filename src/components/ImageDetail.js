b0ce7f19e35ee83f9c8d7450e77407911a192894
kmarks2013 <kmarks2013@gmail.com>
2020-01-23 17:16:58 -0500

    adds basic render for image detail

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ImageDetail = () => {
+    return <Text style={styles.textStyle}>Image Detail</Text>
+}
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 50
+    }
+})
+
+export default ImageDetail
