05ac7e71f2f70ae5ce75bb16588c0fa435626e07
kmarks2013 <kmarks2013@gmail.com>
2020-01-23 17:43:52 -0500

    renders beach image on image detail

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -5,6 +5,7 @@ const ImageDetail = props => {
     // console.log(props)
     return(
         <View>
+            <Image source={require("../../assets/beach.jpg")} />
             <Text>{props.title}</Text>
         </View>
     ) 
