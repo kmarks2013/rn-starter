2e36630e5cdf93e0c291f6e1b7f7e47c317b61d6
kmarks2013 <kmarks2013@gmail.com>
2020-01-23 18:00:17 -0500

    renders out correct image

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -5,7 +5,7 @@ const ImageDetail = props => {
     console.log(props)
     return(
         <View>
-            <Image source={require("../../assets/beach.jpg")} />
+            <Image source={props.imageSource} />
             <Text>{props.title}</Text>
         </View>
     ) 
