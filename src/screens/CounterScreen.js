146067fb6cd6375cd9c7448cd0370e62461e5b6c
kmarks2013 <kmarks2013@gmail.com>
2020-01-24 16:13:58 -0500

    sets up counter in state with default value of 0 and creates setcounter callback function

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { Text, View, StyleSheet, Button} from 'react-native'
 
 const CounterScreen = () => {
-    
+    const [counter, setCounter] = useState(0)
     return (
         <View>
             <Button 
