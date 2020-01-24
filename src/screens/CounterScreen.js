d8a5083f9abd1534a517b56ffb429091363ca06b
kmarks2013 <kmarks2013@gmail.com>
2020-01-24 15:32:35 -0500

    adds counter screen function component

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,2 +1,10 @@
 import React from 'react'
 import { Text, View, StyleSheet, Button} from 'react-native'
+
+const CounterScreen = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
