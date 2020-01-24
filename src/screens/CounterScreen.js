422b0e3e3f421ddac75e2546bb09d3e59ee4f17b
kmarks2013 <kmarks2013@gmail.com>
2020-01-24 15:41:21 -0500

    adds increase button

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -4,6 +4,7 @@ import { Text, View, StyleSheet, Button} from 'react-native'
 const CounterScreen = () => {
     return (
         <View>
+            <Button title="Increase"/>
             <Text>Current Count:</Text>
         </View>
     )
