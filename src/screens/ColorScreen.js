01ae58bca40d48fe5a69da9f8529588b3f741a3e
kmarks2013 <kmarks2013@gmail.com>
2020-02-05 16:21:53 -0500

    creates random color helper method

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -10,6 +10,15 @@ const ColorScreen = () => {
     ) 
 }
 
+const randomRgb = () => {
+    const red = Math.floor(Math.random()*256)
+    const green = Math.floor(Math.random()*256 )
+    const blue = Math.floor(Math.random()*256 )
+
+    return ` rgb( ${red}, ${green}, ${blue})`
+}
+
+
 const styles = StyleSheet.create()
 
 export default ColorScreen
