c49f9241cec92092465539fc935dd8bd3f1b8fa9
kmarks2013 <kmarks2013@gmail.com>
2020-02-05 22:05:09 -0500

    adds stylesheet and exports square screen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,3 +4,7 @@ import {View, Text, StyleSheet} from 'react-native'
 const SquareScreen = () => {
     return <Text>SquareScreen</Text>
 }
+
+const styles = StyleSheet.create({})
+
+export default SquareScreen
