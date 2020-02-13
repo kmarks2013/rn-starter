8018729051fd7c33019b418d98c912e8902197ea
kmarks2013 <kmarks2013@gmail.com>
2020-02-13 16:51:40 -0500

    adds green and blue if thens

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -18,6 +18,18 @@ const SquareScreen = () => {
             } else {
                 setRed(red + change)
             }
+        if (color=== 'green')
+            if (green + change > 255 || green + change < 0){
+                return
+            } else {
+                setGreen(green+change)
+            } 
+        if (color=== 'blue')
+            if (blue + change > 255 || blue + change < 0){
+                return
+            } else {
+                setBlue(blue+change)
+            } 
     }
 
     // console.log(red)
