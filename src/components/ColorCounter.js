ec6dcbd1ab80a98af95ae45e395ccf20015002b6
kmarks2013 <kmarks2013@gmail.com>
2020-02-18 14:30:38 -0500

    renders color prop

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -3,9 +3,9 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 const ColorCounter = ({ color }) => {
     return <View>
-        <Text>Red</Text>
-        <Button title= "Increase Red" />
-        <Button title= "Decrease Red" />
+        <Text>{color}</Text>
+        <Button title= {`Increase ${color}`} />
+        <Button title= {`Decrease ${color}`} />
     </View>
 }
 
