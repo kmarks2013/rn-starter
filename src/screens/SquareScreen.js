c052b9f104e75449b2ec7f8cd78f7538f14b417c
kmarks2013 <kmarks2013@gmail.com>
2020-02-18 16:48:27 -0500

    adds in a color increment to the callbacks to show change faster

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -13,19 +13,19 @@ const SquareScreen = () => {
     return (
         <View>
         <ColorCounter 
-            onIncrease={() => setRed( red + 1 )}
-            onDecrease={() => setRed( red - 1 )}
+            onIncrease={() => setRed( red + COLOR_INCREMENT )}
+            onDecrease={() => setRed( red - COLOR_INCREMENT )}
             color="Red" 
 
         />
         <ColorCounter 
-            onIncrease={() => setBlue( blue + 1 )}
-            onDecrease={() => setBlue( blue - 1 )}
+            onIncrease={() => setBlue( blue + COLOR_INCREMENT )}
+            onDecrease={() => setBlue( blue - COLOR_INCREMENT )}
             color="Blue"
         />
         <ColorCounter
-            onIncrease={() => setGreen( green + 1 )}
-            onDecrease={() => setGreen( green - 1 )} 
+            onIncrease={() => setGreen( green + COLOR_INCREMENT )}
+            onDecrease={() => setGreen( green - COLOR_INCREMENT )} 
             color="Green"
         />
         <View style={{height:150, width:150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}/>
