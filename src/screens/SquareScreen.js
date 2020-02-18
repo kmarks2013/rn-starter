3259b11767d36dee71e1a6c592b68c51815094ed
kmarks2013 <kmarks2013@gmail.com>
2020-02-18 13:53:56 -0500

    alters if else statmente for syntax

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -18,13 +18,13 @@ const SquareScreen = () => {
             } else {
                 setRed(red + change)
             }
-        if (color=== 'green')
+        else if (color=== 'green')
             if (green + change > 255 || green + change < 0){
                 return
             } else {
                 setGreen(green+change)
             } 
-        if (color=== 'blue')
+        else (color=== 'blue')
             if (blue + change > 255 || blue + change < 0){
                 return
             } else {
