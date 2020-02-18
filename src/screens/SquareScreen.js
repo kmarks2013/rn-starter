fc6445332517eaebaae88b78df91b4585ec993dd
kmarks2013 <kmarks2013@gmail.com>
2020-02-18 17:03:03 -0500

    passes setColor funciton into callback for red

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -29,8 +29,8 @@ const SquareScreen = () => {
     return (
         <View>
         <ColorCounter 
-            onIncrease={() => setRed( red + COLOR_INCREMENT )}
-            onDecrease={() => setRed( red - COLOR_INCREMENT )}
+            onIncrease={() => setColor("red", COLOR_INCREMENT)}
+            onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
             color="Red" 
 
         />
