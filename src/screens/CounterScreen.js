a2aed233fac9c6ce79fc58b2ea7dd1bdcf9d6d28
kmarks2013 <kmarks2013@gmail.com>
2020-03-09 20:31:05 -0400

    refactors for count instead of counter  and increment  and decrement

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,34 +2,38 @@ import React, {useReducer} from 'react'
 import { Text, View, StyleSheet, Button} from 'react-native'
 
 const  reducer = (state, action) => {
+    //  state ==={count : 0}
+    //  action === type : "increment" || "decrement" payload :1
     switch(action.type){
-        case 'change_counter': 
-            return {...state, counter: state.counter + action.payload }
-        defalut:
-            return  state
+        case 'increment': 
+            return {...state, count: state.count + action.payload }
+        case 'decrement': 
+            return {...state, count: state.count - action.payload }
+        default:
+            return state
     }
 } 
 
 const CounterScreen = () => {
     
-    const [state, dispatch] = useReducer(reducer, {counter:0})
-    const {counter} = state
+    const [state, dispatch] = useReducer(reducer, {count:0})
+    const {count} = state
  
     return (
         <View>
             <Button
                 title="Increase"
                 onPress={() =>
-                    dispatch({type:'change_counter', payload: 1})
+                    dispatch({type:'increment', payload: 1})
                 }
             />
             <Button 
                 title="Decrease"
                 onPress={() =>
-                    dispatch({type:'change_counter', payload: -1})
+                    dispatch({type:'decrement', payload: 1})
                 }
              />
-            <Text>Current Count: {counter} </Text>
+            <Text>Current Count: {count} </Text>
         </View>
     )
 }
