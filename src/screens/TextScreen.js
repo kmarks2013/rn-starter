2fb6d1be8e2a269a373ec1610eef75e27fd2c7b6
kmarks2013 <kmarks2013@gmail.com>
2020-03-23 18:41:05 -0400

    adds Text Screen with basic render

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TextScreen = () => {
+    return (
+        <View>
+            <Text>Sample Text</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default TextScreen
+
